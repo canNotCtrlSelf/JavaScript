a1dcdcfd1afd50aaf213ee90ab6fe64ca4ae908c
canNotCtrlSelf <xiaochunhuo132@163.com>
2025-08-22 23:20:51 +0800

    memories

---
+++ 20250822/js/script.js
@@ -0,0 +1,12 @@
+// 输出内容到控制台
+console.log("Hello, JavaScript!");
+
+// 定义变量
+let name = "Nick";     // 字符串
+let age = 25;          // 数字
+let isStudent = true;  // 布尔值
+
+// 输出变量
+console.log("姓名:", name);
+console.log("年龄:", age);
+console.log("是学生吗:", isStudent);
